b74c5bbf80dc1ebf6c4bd2901417a5c7358d8298
Andrea <Andrea@DESKTOP-LE1HDE8>
2021-08-10 01:45:58 -0300

    Agregar archivos de proyecto.

---
+++ TurismoVS/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TurismoVS.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
